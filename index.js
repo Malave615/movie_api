ca39c8f3b938deb550494871f79411941052f863
Malave615 <malavetracy@gmail.com>
2024-05-15 13:22:09 -0400

    Updated index.js
    Deleted body-parser as it is included in updated version of express. Deleted the movie array that was saved internally in order to test database.

--- index.js
+++ index.js
@@ -1,88 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 
+const mongoose = require('mongoose');
+
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a',
 });
 
-const movies = [
-  {
-    title: 'My Fair Lady',
-    year: 1964,
-    director: 'George Cukor',
-    genre: 'Musical',
-  },
-  {
-    title: 'Gone With The Wind',
-    year: 1939,
-    director: 'Victor Fleming',
-    genre: 'Drama',
-  },
-  {
-    title: 'Memoirs of a Geisha',
-    year: 2005,
-    director: 'Rbo Marshall',
-    genre: 'Drama',
-  },
-  {
-    title: 'Forrest Gump',
-    year: 1994,
-    director: 'Robert Zemeckis',
-    genre: 'Drama',
-  },
-  {
-    title: 'Shawshank Redemption',
-    year: 1994,
-    director: 'Frank Darabont',
-    genre: 'Drama',
-  },
-  {
-    title: 'The Godfather',
-    year: 1972,
-    director: 'Francis Ford Coppola',
-    genre: 'Drama',
-  },
-  {
-    title: 'Goodfellas',
-    year: 1990,
-    director: 'Martin Scorsese',
-    genre: 'Drama',
-  },
-  {
-    title: "Breakfast at Tiffany's",
-    year: 1961,
-    director: 'Blake Edwards',
-    genre: 'Romance',
-  },
-  {
-    title: 'The Notebook',
-    year: 2004,
-    director: 'Nick Cassavetes',
-    genre: 'Romance',
-  },
-  {
-    title: 'Miracle on 34th Street',
-    year: 1947,
-    director: 'George Seaton',
-    genre: 'Family',
-  },
-];
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(morgan('combined', { stream: accessLogStream }));
 
-// READ
+// Get list of all movies
 app.get('/movies', (req, res) => {
-  res.status(200).send('Successful GET request returning data on all movies');
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-// READ
+// Get a movie by title
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
   const movie = movies.find((m) => m.title === title);
@@ -97,52 +54,152 @@ app.get('/', (req, res) => {
   res.send('Welcome to my Movie API!');
 });
 
+// Get list of movies by genre
 app.get('/movies/genres/{genreName}', (req, res) => {
   res.send(
     'Successful GET request returning data on a movies with the corresponding genre',
   );
 });
 
+// Get list of movies by director
 app.get('/movies/directors/{directorName}', (req, res) => {
   res.send(
     'Successful GET request returning data on movies by the corresponding director',
   );
 });
 
+// Get list of all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
+});
+
+// Update a user's info, by username
+app.put('/users/:Username', async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true },
+  )
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
+});
+
+// User login
 app.post('/login', (req, res) => {
   const newUser = req.body;
   newUser.id = uuid.v4();
   res.send('Successful POST request returning an authentication token');
 });
 
-app.post('/users', (req, res) => {
-  res
-    .status(200)
-    .send('Successful POST request returning a confirmation message');
+// Register a new user
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(`${req.body.Username}already exists`);
+      }
+      Users.create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+        FavoriteMovies: req.body.FavoriteMovies,
+      })
+        .then((user) => {
+          res.status(201).json(user);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send(`Error: ${error}`);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send(`Error: ${error}`);
+    });
 });
 
-app.put('/users/:username', (req, res) => {
-  res
-    .status(200)
-    .send('Successful PUT request returning a confirmation message');
+// Adda movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true },
+  ) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-app.delete('/users/:username', (req, res) => {
-  res
-    .status(200)
-    .send('Successful DELETE request returning a confirmation message');
+// Remove a movie from a user's list of favorites
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true },
+  ) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-app.put('/users/:username/movies/:movieID', (req, res) => {
-  res
-    .status(200)
-    .send('Successful PUT request returning a confirmation message');
-});
-
-app.delete('/users/:username/movies/:movieID', (req, res) => {
-  res
-    .status(200)
-    .send('Successful DELETE request returning a confirmation message');
+// Delete a user by username
+app.delete('/users/:Username', async (req, res) => {
+  await Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(`${req.params.Username} was not found`);
+      } else {
+        res.status(200).send(`${req.params.Username} was deleted.`);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
 app.use(express.static('public'));
