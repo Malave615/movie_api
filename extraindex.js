9eaa2f72599f9b6df411cac10045c67cfd4b3cc2
Malave615 <malavetracy@gmail.com>
2024-05-15 13:17:43 -0400

    Back up data
    The contents of my movie api that were saved internally before database was created.

---
+++ extraindex.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+const movies = [
+  {
+    title: 'My Fair Lady',
+    year: 1964,
+    director: 'George Cukor',
+    genre: 'Musical',
+  },
+  {
+    title: 'Gone With The Wind',
+    year: 1939,
+    director: 'Victor Fleming',
+    genre: 'Drama',
+  },
+  {
+    title: 'Memoirs of a Geisha',
+    year: 2005,
+    director: 'Rbo Marshall',
+    genre: 'Drama',
+  },
+  {
+    title: 'Forrest Gump',
+    year: 1994,
+    director: 'Robert Zemeckis',
+    genre: 'Drama',
+  },
+  {
+    title: 'Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    genre: 'Drama',
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    genre: 'Drama',
+  },
+  {
+    title: 'Goodfellas',
+    year: 1990,
+    director: 'Martin Scorsese',
+    genre: 'Drama',
+  },
+  {
+    title: "Breakfast at Tiffany's",
+    year: 1961,
+    director: 'Blake Edwards',
+    genre: 'Romance',
+  },
+  {
+    title: 'The Notebook',
+    year: 2004,
+    director: 'Nick Cassavetes',
+    genre: 'Romance',
+  },
+  {
+    title: 'Miracle on 34th Street',
+    year: 1947,
+    director: 'George Seaton',
+    genre: 'Family',
+  },
+];
