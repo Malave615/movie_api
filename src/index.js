3f107ab03063df4ce4383a825b1751cb51edc064
Malave615 <malavetracy@gmail.com>
2024-09-06 15:12:28 -0400

    Reloaded collections
    Added a 'ImagePath' attribute to my movies collection and pushed changes to MongoAtlas.

--- src/index.js
+++ src/index.js
@@ -43,15 +43,15 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a',
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-/* mongoose.connect(process.env.CONNECTION_URI, {
+/* mongoose.connect('mongodb://127.0.0.1:27017/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }); */
+
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(morgan('combined', { stream: accessLogStream }));
 
