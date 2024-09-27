e8af1851b480271ed64131feca14277cda02b8a9
Malave615 <malavetracy@gmail.com>
2024-09-27 12:27:36 -0400

    CORS
    Added preflight stipulations for handling CORS options.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,24 @@ app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const message = `The CORS policy for this application does not allow access from origin ${origin}`;
+      return callback(new ErrorEvent(message), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Allow preflight for all routes
+app.options('*', cors());
+
 const allowedOrigins = [
   'http://localhost:8080',
   'http://testsite.com',
